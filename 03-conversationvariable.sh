e5be88cc8770c6cd65fbbf3bfda45c03408ddbd3
naginenihari <naginenihari@gmail.com>
2026-02-02 10:12:26 +0530

    Variables statements are updated

--- 03-conversationvariable.sh
+++ 03-conversationvariable.sh
@@ -10,6 +10,6 @@ PERSON1=Hari
 PERSON2=Babu
 
 echo "$PERSON1:: How are you $PERSON2"
-echo "$PERSON2:: am fine what about you $PERSON1"
+echo "$PERSON2:: Am fine what about you $PERSON1"
 echo "$PERSON1:: what are you doing am learning devsecops $PERSON2"
 echo "$PERSON2:: fine am also learning devsecops $PERSON1"
