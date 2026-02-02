354e73423b257f7ad32ea59c36ca2a69a02d116a
naginenihari <naginenihari@gmail.com>
2026-02-02 10:06:58 +0530

    Conversion statements are updated

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,5 +2,5 @@
 
 echo "Ramesh:: How are you"
 echo "Suresh:: am fine what about you"
-echo "Ramesh:: ok what are you doing am learning devsecops suresh"
-echo "Sures:: ok fine am also learning devsecops Ramesh"
+echo "Ramesh:: ok what are you doing am learning devsecops Suresh"
+echo "Suresh:: ok fine am also learning devsecops Ramesh"
