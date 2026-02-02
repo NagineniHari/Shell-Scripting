bc785d1f8ae908a749a3e9905efbeef11fe26acb
naginenihari <naginenihari@gmail.com>
2026-02-02 10:59:27 +0530

    Environment Variable are updated

--- 06-Enviroment_Variable.sh
+++ 06-Enviroment_Variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "AP CM name is $CM"
-echo "AP DCM name is $DCM"
+echo "AP CM name is: $CM"
+echo "AP DCM name is: $DCM"
 
-echo "TN CM name is $CM"
-echo "TN DCM name is $DCM"
+echo "TN CM name is: $CM"
+echo "TN DCM name is: $DCM"
